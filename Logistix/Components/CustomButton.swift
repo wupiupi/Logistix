aeb9025124c8f63f79705423258d9a9247d1470a
Paul <pasha.makey@gmail.com>
2024-05-12 00:23:10 +0300

    Extracted View for Button

--- Logistix/Components/CustomButton.swift
+++ Logistix/Components/CustomButton.swift
@@ -5,4 +5,46 @@
 //  Created by Paul Makey on 12.05.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomButton: View {
+    let title: String
+    var systemImage: String = ""
+    let onClick: () -> ()
+    
+    var body: some View {
+        Button {
+            onClick()
+        } label: {
+            HStack {
+                Text(title)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                Image(systemName: systemImage)
+                    .imageScale(.small)
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .padding(.trailing, 20)
+            }
+            .hAlign(alignment: .leading)
+            .padding(.top, 15)
+            .padding(.bottom, 10)
+            .padding(.leading, 20)
+            .foregroundStyle(.white)
+            .background {
+                RoundedRectangle(cornerRadius: 25)
+                    .foregroundStyle(.mint)
+            }
+        }
+        .padding([.bottom, .horizontal])
+    }
+}
+
+#Preview {
+    CustomButton(
+        title: "Start",
+        systemImage: "arrowshape.right.fill") {}
+}
