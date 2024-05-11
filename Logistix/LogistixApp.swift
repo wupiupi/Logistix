49b786947a7d89c2232b07ad54be5e481f320c76
Paul <pasha.makey@gmail.com>
2024-05-11 22:20:29 +0300

    Initial Commit

---
+++ Logistix/LogistixApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogistixApp.swift
+//  Logistix
+//
+//  Created by Paul Makey on 11.05.24.
+//
+
+import SwiftUI
+
+@main
+struct LogistixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
