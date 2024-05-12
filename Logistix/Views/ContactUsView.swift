5542c18475450bb501e02cdb7a91d444c83ff825
Paul <pasha.makey@gmail.com>
2024-05-12 18:06:59 +0300

    Created new Files

---
+++ Logistix/Views/ContactUsView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactUsView.swift
+//  Logistix
+//
+//  Created by Paul Makey on 12.05.24.
+//
+
+import SwiftUI
+
+struct ContactUsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactUsView()
+}
