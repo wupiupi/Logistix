4257c391df7ad6ebf63221d088f781e026c8add6
Paul <pasha.makey@gmail.com>
2024-05-12 18:07:32 +0300

    Made Extracted View for Input Fields

--- Logistix/Components/InputView.swift
+++ Logistix/Components/InputView.swift
@@ -5,4 +5,55 @@
 //  Created by Paul Makey on 12.05.24.
 //
 
-import Foundation
+import SwiftUI
+
+struct InputView: View {
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecureField: Bool = false
+    
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text(title)
+                .foregroundStyle(.black)
+                .font(.title3)
+            
+            TextField(placeholder, text: $text)
+                .foregroundStyle(.white)
+                .frame(
+                    width: UIScreen.main.bounds.width - 82,
+                    height: 50
+                )
+                .padding(.leading, 16)
+                .background {
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundStyle(
+                            Color(
+                                hex: 0xF9F9F9,
+                                alpha: 1
+                            )
+                        )
+                        .background {
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(
+                                    Color(
+                                        hex: 0xDDDDDD,
+                                        alpha: 1
+                                    ),
+                                    lineWidth: 3
+                                )
+                        }
+                }
+        }
+    }
+}
+
+#Preview {
+    InputView(
+        text: .constant(""),
+        title: "Имя *",
+        placeholder: "Полное имя*"
+    )
+}
