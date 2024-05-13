325a01669d056a40510b3b3d01d4d2c0da3e9f2a
bowski43 <43mngt@gmail.com>
2024-05-14 01:46:12 +0300

    initial setup

---
+++ Logistix/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Logistix
+//
+//  Created by Serge Broski on 5/14/24.
+//
+
+import Foundation
+
+struct User {
+    let user: String
+    let pass: String
+    let orders: [Order]
+}
