639ea995f3fca92dd2f85a94621e5804d96c8e98
Paul <pasha.makey@gmail.com>
2024-05-14 13:46:37 +0300

    Added id, trackingNumber & status Fields

--- Logistix/Models/Order.swift
+++ Logistix/Models/Order.swift
@@ -7,7 +7,10 @@
 
 import Foundation
 
-struct Order {
+struct Order: Hashable {
+    let id: String
+    let trackingNumber = String.generateTrackNum()
+    let status: String
     let sourceAddress: String
     let destinationAddress: String
     let senderName: String
@@ -22,21 +25,42 @@ struct Order {
     let payment: String
     let totalCost: String
     
-    static func MOCK_ORDER() -> Order {
-        Order(
-            sourceAddress: "Притыцкого 12",
-            destinationAddress: "Независимости 124",
-            senderName: "Павел",
-            senderPhoneNumber: "+375(25)444-44-55",
-            recipientName: "Олег",
-            recipientPhoneNumber: "+375(25)111-22-33",
-            cargoType: "Скоропортящиеся продукты",
-            weight: "100кг",
-            dateOfLoading: Date(),
-            dateOfDelivery: Date(),
-            cargoCost: "200",
-            payment: "card",
-            totalCost: "10000"
-        )
+    static func MOCK_ORDERS() -> [Order] {
+        [
+            Order(
+                id: "123456",
+                status: "Ожидает подтверждения",
+                sourceAddress: "Притыцкого, 12",
+                destinationAddress: "Независимости, 124",
+                senderName: "Павел",
+                senderPhoneNumber: "+375(25)444-44-55",
+                recipientName: "Олег",
+                recipientPhoneNumber: "+375(25)111-22-33",
+                cargoType: "скоропортящихся продуктов",
+                weight: "100кг",
+                dateOfLoading: Date(),
+                dateOfDelivery: Date(),
+                cargoCost: "200",
+                payment: "card",
+                totalCost: "10000"
+            ),
+            Order(
+                id: "789212",
+                status: "В пути",
+                sourceAddress: "Пушкинская, 12",
+                destinationAddress: "Независимости 124",
+                senderName: "Геральд",
+                senderPhoneNumber: "+375(25)476-53-22",
+                recipientName: "Йениффер",
+                recipientPhoneNumber: "+375(29)062-83-93",
+                cargoType: "строительных материалов",
+                weight: "100кг",
+                dateOfLoading: Date(),
+                dateOfDelivery: Date(),
+                cargoCost: "200",
+                payment: "card",
+                totalCost: "10000"
+            )
+        ]
     }
 }
