17feb66753d15e097289df64d501250ace67c13f
Paul <pasha.makey@gmail.com>
2024-05-16 00:07:59 +0300

    Firebase Configured

--- Logistix/App/LogistixApp.swift
+++ Logistix/App/LogistixApp.swift
@@ -13,7 +13,12 @@ let realmApp = RealmSwift.App(id: "application-0-tqodywz")
 
 @main
 struct LogistixApp: SwiftUI.App {
-    @StateObject private var ordersVM = OrdersViewModel()
+    @StateObject private var viewModel = AuthViewModel()
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView(app: realmApp)
