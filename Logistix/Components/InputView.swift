c42415011bde2643f7f4294bf97488ff531e29d2
Paul <pasha.makey@gmail.com>
2024-05-19 15:43:18 +0300

    Secure Field Checking

--- Logistix/Components/InputView.swift
+++ Logistix/Components/InputView.swift
@@ -20,34 +20,64 @@ struct InputView: View {
                 .foregroundStyle(Color(hex: 0x363746, alpha: 1))
                 .font(.title3)
             
-            TextField(placeholder, text: $text)
-                .foregroundStyle(.black)
-                .keyboardType(isNumPad ? .numberPad : .default)
-                .frame(
-                    width: UIScreen.main.bounds.width - 80,
-                    height: 50
-                )
-                .padding(.leading, 16)
-                .background {
-                    RoundedRectangle(cornerRadius: 20)
-                        .shadow(radius: 3, x: 0, y: -1)
-                        .foregroundStyle(
-                            Color(
-                                hex: 0xF9F9F9,
-                                alpha: 1
-                            )
-                        )
-                        .background {
-                            RoundedRectangle(cornerRadius: 20)
-                                .stroke(
-                                    Color(
-                                        hex: 0xDDDDDD,
-                                        alpha: 1
-                                    ),
-                                    lineWidth: 3
+            if isSecureField {
+                SecureField(placeholder, text: $text)
+                    .foregroundStyle(.black)
+                    .frame(
+                        width: UIScreen.main.bounds.width - 80,
+                        height: 50
+                    )
+                    .padding(.leading, 16)
+                    .background {
+                        RoundedRectangle(cornerRadius: 20)
+                            .shadow(radius: 3, x: 0, y: -1)
+                            .foregroundStyle(
+                                Color(
+                                    hex: 0xF9F9F9,
+                                    alpha: 1
                                 )
-                        }
-                }
+                            )
+                            .background {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(
+                                        Color(
+                                            hex: 0xDDDDDD,
+                                            alpha: 1
+                                        ),
+                                        lineWidth: 3
+                                    )
+                            }
+                    }
+            } else {
+                TextField(placeholder, text: $text)
+                    .foregroundStyle(.black)
+                    .keyboardType(isNumPad ? .numberPad : .default)
+                    .frame(
+                        width: UIScreen.main.bounds.width - 80,
+                        height: 50
+                    )
+                    .padding(.leading, 16)
+                    .background {
+                        RoundedRectangle(cornerRadius: 20)
+                            .shadow(radius: 3, x: 0, y: -1)
+                            .foregroundStyle(
+                                Color(
+                                    hex: 0xF9F9F9,
+                                    alpha: 1
+                                )
+                            )
+                            .background {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(
+                                        Color(
+                                            hex: 0xDDDDDD,
+                                            alpha: 1
+                                        ),
+                                        lineWidth: 3
+                                    )
+                            }
+                    }
+            }
         }
     }
 }
