dc35367b8325959c6923368c4d33d52e83da92ac
Paul <pasha.makey@gmail.com>
2024-05-19 15:46:02 +0300

    Custom Modifier for Input View Added

--- Logistix/Components/InputView.swift
+++ Logistix/Components/InputView.swift
@@ -22,70 +22,44 @@ struct InputView: View {
             
             if isSecureField {
                 SecureField(placeholder, text: $text)
-                    .foregroundStyle(.black)
-                    .frame(
-                        width: UIScreen.main.bounds.width - 80,
-                        height: 50
-                    )
-                    .padding(.leading, 16)
-                    .background {
-                        RoundedRectangle(cornerRadius: 20)
-                            .shadow(radius: 3, x: 0, y: -1)
-                            .foregroundStyle(
-                                Color(
-                                    hex: 0xF9F9F9,
-                                    alpha: 1
-                                )
-                            )
-                            .background {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .stroke(
-                                        Color(
-                                            hex: 0xDDDDDD,
-                                            alpha: 1
-                                        ),
-                                        lineWidth: 3
-                                    )
-                            }
-                    }
+                    .modifier(InputViewModifier())
             } else {
                 TextField(placeholder, text: $text)
-                    .foregroundStyle(.black)
                     .keyboardType(isNumPad ? .numberPad : .default)
-                    .frame(
-                        width: UIScreen.main.bounds.width - 80,
-                        height: 50
-                    )
-                    .padding(.leading, 16)
-                    .background {
-                        RoundedRectangle(cornerRadius: 20)
-                            .shadow(radius: 3, x: 0, y: -1)
-                            .foregroundStyle(
-                                Color(
-                                    hex: 0xF9F9F9,
-                                    alpha: 1
-                                )
-                            )
-                            .background {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .stroke(
-                                        Color(
-                                            hex: 0xDDDDDD,
-                                            alpha: 1
-                                        ),
-                                        lineWidth: 3
-                                    )
-                            }
-                    }
+                    .modifier(InputViewModifier())
             }
         }
     }
 }
 
-#Preview {
-    InputView(
-        text: .constant(""),
-        title: "Имя *",
-        placeholder: "Полное имя*"
-    )
+struct InputViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(.black)
+            .frame(
+                width: UIScreen.main.bounds.width - 80,
+                height: 50
+            )
+            .padding(.leading, 16)
+            .background {
+                RoundedRectangle(cornerRadius: 20)
+                    .shadow(radius: 3, x: 0, y: -1)
+                    .foregroundStyle(
+                        Color(
+                            hex: 0xF9F9F9,
+                            alpha: 1
+                        )
+                    )
+                    .background {
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(
+                                Color(
+                                    hex: 0xDDDDDD,
+                                    alpha: 1
+                                ),
+                                lineWidth: 3
+                            )
+                    }
+            }
+    }
 }
