7ed60bec4c94b32d6541a3729c5a6c8307c1d957
Paul <pasha.makey@gmail.com>
2024-06-02 18:25:26 +0300

    Made New View

---
+++ Logistix/Views/OrderReport/OrderReportView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderReportView.swift
+//  Logistix
+//
+//  Created by Paul Makey on 2.06.24.
+//
+
+import SwiftUI
+
+struct OrderReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderReportView()
+}
