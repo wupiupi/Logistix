77b1cb88ea957bbf1482d2333e37d53dff9863eb
Paul <pasha.makey@gmail.com>
2024-06-02 18:28:12 +0300

    Embed in Navigation Stack

--- Logistix/Views/Orders/OrdersView/Subviews/TrackInfoView.swift
+++ Logistix/Views/Orders/OrdersView/Subviews/TrackInfoView.swift
@@ -12,90 +12,114 @@ struct TrackInfoView: View {
     @EnvironmentObject private var ordersVM: OrdersViewModel
     
     let order: Order
-    
+
     var body: some View {
-        VStack(alignment: .center, spacing: 20) {
-            Text(order.cargoType)
-                .modifier(TitleModifier(font: .title, fontWeight: .bold))
-            
-            OrderDetailsView(
-                title: "Адрес отправителя",
-                orderInfo: order.route?.sourceAddress ?? ""
-            )
-            OrderDetailsView(
-                title: "Адрес получателя",
-                orderInfo: order.route?.destinationAddress ?? ""
-            )
-            OrderDetailsView(
-                title: "Поставщик",
-                orderInfo: order.sender?.name ?? ""
-            )
-            OrderDetailsView(
-                title: "Контактный телефон",
-                orderInfo: order.sender?.phoneNumber ?? ""
-            )
-            OrderDetailsView(
-                title: "Трек номер",
-                orderInfo: order.trackingNumber,
-                systemImageName: "doc.on.doc.fill"
-            )
-            OrderDetailsView(
-                title: "Стоимость",
-                orderInfo: order.price?.totalCost ?? ""
-            )
-            
-            Text("Статус заказа")
-                .font(.title3)
-                .foregroundStyle(.gray)
-            
-            Text(order.status)
-                .font(.title3)
-                .foregroundStyle(
-                    order.status == "Подтвержден"
-                    ? Color(hex: 0x00CCA6, alpha: 1)
-                    : .red
-                )
-                .padding([.top, .bottom], 8)
-                .padding([.leading, .trailing], 8)
-                .background {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(order.status == "Подтвержден"
-                              ? .main.opacity(0.2)
-                              : .red.opacity(0.2)
+        NavigationStack {
+            VStack(alignment: .center, spacing: 20) {
+                Text(order.cargoType)
+                    .modifier(
+                        TitleModifier(
+                            font: .title,
+                            fontWeight: .bold,
+                            color: Color(hex: 0x363746, alpha: 1)
                         )
-                }
-            
-            if authVM.currentUser?.role == Role.admin.rawValue {
-                OrderButtonView(
-                    title: "Подтвердить заказ",
-                    titleColor: .white,
-                    backColor: .green) {
-                        ordersVM.storageManager.write {
-                            order.thaw()?.status = "Подтвержден"
-                        }
-                    }
-                    .disabled(order.status == "Подтвержден")
-                    .opacity(order.status == "Подтвержден" ? 0.4 : 1)
+                    )
                 
-                OrderButtonView(
-                    title: "Отменить",
-                    titleColor: .red,
-                    backColor: .clear) {
-                        ordersVM.storageManager.write {
-                            order.thaw()?.status = "Отменен"
-                        }
+                OrderDetailsView(
+                    title: "Адрес отправителя",
+                    orderInfo: order.route?.sourceAddress ?? ""
+                )
+                OrderDetailsView(
+                    title: "Адрес получателя",
+                    orderInfo: order.route?.destinationAddress ?? ""
+                )
+                OrderDetailsView(
+                    title: "Поставщик",
+                    orderInfo: order.sender?.name ?? ""
+                )
+                OrderDetailsView(
+                    title: "Контактный телефон",
+                    orderInfo: order.sender?.phoneNumber ?? ""
+                )
+                OrderDetailsView(
+                    title: "Трек номер",
+                    orderInfo: order.trackingNumber,
+                    systemImageName: "doc.on.doc.fill"
+                )
+                OrderDetailsView(
+                    title: "Стоимость",
+                    orderInfo: order.price?.totalCost ?? ""
+                )
+                
+                Text("Статус заказа")
+                    .font(.title3)
+                    .foregroundStyle(.gray)
+                
+                Text(order.status)
+                    .font(.title3)
+                    .foregroundStyle(
+                        order.status == "Отменен"
+                        ? .red
+                        : Color(hex: 0x00CCA6, alpha: 1)
+                    )
+                    .padding([.top, .bottom], 8)
+                    .padding([.leading, .trailing], 8)
+                    .background {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(order.status == "Отменен"
+                                  ? .red.opacity(0.2)
+                                  : .main.opacity(0.2)
+                            )
                     }
-                    .disabled(order.status == "Отменен")
-                    .opacity(order.status == "Отменен" ? 0.4 : 1)
+                
+                if authVM.currentUser?.role == Role.admin.rawValue {
+                    
+                    switch order.status {
+                        case "Завершён":
+                            NavigationLink {
+                                OrderReportView(order: order)
+                            } label: {
+                                Text("Посмотреть отчет")
+                                    .font(.title2)
+                                    .foregroundStyle(.white)
+                                    .padding()
+                                    .background {
+                                        RoundedRectangle(cornerRadius: 12)
+                                            .fill(.green)
+                                    }
+                            }
+                        case "Отменен":
+                            OrderButtonView(
+                                title: "Подтвердить заказ",
+                                titleColor: .white,
+                                backColor: .green) {
+                                    ordersVM.storageManager.write {
+                                        order.thaw()?.status = "Подтвержден"
+                                    }
+                                }
+                        default:
+                            OrderButtonView(
+                                title: "Завершить",
+                                titleColor: .white,
+                                backColor: .green) {
+                                    ordersVM.storageManager.write {
+                                        order.thaw()?.status = "Завершён"
+                                    }
+                                }
+                            
+                            OrderButtonView(
+                                title: "Отменить",
+                                titleColor: .red,
+                                backColor: .clear) {
+                                    ordersVM.storageManager.write {
+                                        order.thaw()?.status = "Отменен"
+                                    }
+                                }
+                    }
+                }
             }
         }
         .padding()
         .hAlign(.center)
     }
-}
-
-#Preview {
-    TrackInfoView(
-        order: Order()
-    )
 }
