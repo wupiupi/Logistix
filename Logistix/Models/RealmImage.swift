dce6d59ed30e888ddd20958695698eb476e8a6dd
Paul <pasha.makey@gmail.com>
2025-02-21 12:07:31 +0300

    Integrated Realm dependencies. Added Image model for storing Image data in Realm. Added Realm StorageManager

---
+++ Logistix/Models/RealmImage.swift
@@ -0,0 +1,21 @@
+//
+//  RealmImage.swift
+//  Logistix
+//
+//  Created by Paul Makey on 20.02.25.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmImage: Object, Identifiable {
+    @Persisted var id: String = UUID().uuidString
+    @Persisted var imageID: String
+    @Persisted var data: Data?
+    
+    convenience init(imageID: String, data: Data?) {
+        self.init()
+        self.imageID = imageID
+        self.data = data
+    }
+}
---
+++ Logistix/Services/StorageManager.swift
@@ -0,0 +1,56 @@
+//
+//  StorageManager.swift
+//  Logistix
+//
+//  Created by Paul Makey on 20.02.25.
+//
+
+import Foundation
+import RealmSwift
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    private let realm: Realm
+    
+    private init() {
+        
+        if let realmURL = Realm.Configuration.defaultConfiguration.fileURL {
+                    let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+                    let fileURL = documentDirectory?.appendingPathComponent(realmURL.lastPathComponent)
+                    print("Realm file URL: \(fileURL?.path ?? "")")
+                }
+        
+        do {
+            realm = try Realm()
+        } catch {
+            fatalError("Failed to initialize Realm: \(error)")
+        }
+    }
+    
+    // MARK: - CRUD
+    
+    // Update
+    func fetchData<T: RealmFetchable>(_ type: T.Type) -> Results<T> {
+        realm.objects(T.self)
+    }
+    
+    // Save new one
+    func save<T: Object>(_ object: T, completion: (T) -> Void) {
+        write {
+            realm.add(object)
+            completion(object)
+        }
+    }
+    
+    // Write changes
+    func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
