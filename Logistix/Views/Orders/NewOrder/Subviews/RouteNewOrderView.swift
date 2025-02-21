6dcef20495bb9e37aa246d1d2439ffdad12f3040
Paul <pasha.makey@gmail.com>
2025-02-21 12:11:50 +0300

    Beta version of PhotosPicker implementation in creation of a new order

--- Logistix/Views/Orders/NewOrder/Subviews/RouteNewOrderView.swift
+++ Logistix/Views/Orders/NewOrder/Subviews/RouteNewOrderView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct RouteNewOrderView: View {
     @EnvironmentObject private var newOrderVM: NewOrderViewModel
@@ -17,6 +18,27 @@ struct RouteNewOrderView: View {
                 .fontWeight(.bold)
                 .foregroundStyle(Color(hex: 0x363746, alpha: 1))
                 .padding(.leading)
+            
+            VStack {
+                PhotosPicker(selection: $newOrderVM.selectedItem, matching: .images) {
+                    Text("Выбрать фото")
+                }
+                .onChange(of: newOrderVM.selectedItem) { _, newItem in
+                    Task {
+                        if let data = try? await newItem?.loadTransferable(type: Data.self) {
+                            newOrderVM.selectedImage = UIImage(data: data)
+                            newOrderVM.imageData = newOrderVM.selectedImage?.jpegData(compressionQuality: 0.8)
+                        }
+                    }
+                }
+                
+                if let selectedImage = newOrderVM.selectedImage {
+                    Image(uiImage: selectedImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 200)
+                }
+            }
             
             VStack {
                 InputView(
