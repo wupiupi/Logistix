8eac0949796403f7524b611e185edf5de2e7c29e
Paul <pasha.makey@gmail.com>
2025-02-28 14:19:06 +0300

    Removed blank spaces

--- Logistix/Views/Orders/NewOrder/NewOrderView.swift
+++ Logistix/Views/Orders/NewOrder/NewOrderView.swift
@@ -7,42 +7,34 @@ struct NewOrderView: View {
                 VStack(alignment: .leading) {
                     // MARK: - Header
                     HeaderNewOrderView()
-                    
                     Divider()
                     
                     // MARK: - Route
                     RouteNewOrderView()
-                    
                     Divider()
                     
                     // MARK: - Contact info
                     ContactNewOrderView()
-                    
                     Divider()
                     
                     // MARK: - Carrier info
                     CarrierNewOrderView()
-                    
                     Divider()
                     
                     // MARK: - Estimated time
                     EstimatedTimeNewOrderView()
-                    
                     Divider()
                     
                     // MARK: - Image Picker
                     SelectImageView()
-                    
                     Divider()
                     
                     // MARK: - Worth of order
                     WorthNewOrderView()
-                    
                     Divider()
                     
                     // MARK: - Payment method
                     PaymentMethodNewOrderView()
-                    
                     Divider()
                     
                     // MARK: - In total
