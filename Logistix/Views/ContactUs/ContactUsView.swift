bb1a1d9e604704819875e0f55000f0a306f27009
Paul <pasha.makey@gmail.com>
2025-02-28 14:18:54 +0300

    Added Navigation Title and Scroll View

--- Logistix/Views/ContactUs/ContactUsView.swift
+++ Logistix/Views/ContactUs/ContactUsView.swift
@@ -2,15 +2,15 @@ import SwiftUI
 
 struct ContactUsView: View {
     var body: some View {
-        VStack {
-            Text("Связаться с нами")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .padding(.bottom, 6)
-            
-            ContactUsFieldsView()
-            
-            ContactUsButtonView()
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    ContactUsFieldsView()
+                    ContactUsButtonView()
+                }
+                .padding()
+                .navigationTitle("Связаться с нами")
+            }
         }
     }
 }
